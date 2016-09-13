61c622226e726838eef4cc53bbb56aeb626db372
Danilo Carandang <jrcaranddang@gmail.com>
2016-09-13 00:48:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+describe('index', () => {
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+  })
+
+  describe('changeCompletely(element, index, array)', () => {
+    it('is defined', () => {
+      expect(changeCompletely).to.be.a('function')
+    })
+
+    it('completely alters an array in place when used with Array.prototype.forEach', () => {
+      const array = [1, 2, 3]
+
+      expect(array.forEach(changeCompletely)).not.to.eql([1, 2, 3])
+    })
+  })
+*/
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+
+
+/*
+  describe('doToElementsInArray(array, callback)', () => {
+    it('is defined', () => {
+      expect(doToElementsInArray).to.be.a('function')
+    })
+
+    it('performs `callback` on `array` using `Array.prototype.forEach`', () => {
+      const callback = chai.spy()
+      const array = [1, 2, 3]
+      const forEach = chai.spy.on(array, 'forEach')
+
+      doToElementsInArray(array, callback)
+
+      expect(callback).to.have.been.called
+      expect(forEach).to.have.been.called
+    })
+  })
+})
+*/
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+var fruits = ['apples', 'bannanas', 'oranges'];
+
+doToElementsInArray(fruits, changeCompletely);
